e3f6db870df9b1588e5e57555d414605fec7306f
Nico-Battaglia <nico-battaglia@hotmail.com>
2023-09-09 19:39:04 -0300

    Add navbar with search functionality and a results page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
+import Navbar from "./components/Navbar"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -13,6 +14,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Navbar />
         <main className="max-w-6xl mx-auto p-2">{children}</main>
       </body>
     </html>
---
+++ src/app/results/[search]/page.tsx
@@ -0,0 +1,54 @@
+import fetchImages from "@/lib/fetchImages"
+import Image from "next/image"
+
+interface PageProps {
+  params: { search: string }
+}
+
+export function generateMetadata({ params: { search } }: PageProps) {
+  let searchFormatted = ""
+
+  search.includes("%20")
+    ? (searchFormatted = search.split("%20").join(" "))
+    : (searchFormatted = search)
+
+  return {
+    title: `Results for ${searchFormatted}`,
+  }
+}
+
+export default async function Home({ params: { search } }: PageProps) {
+  const imagesResults = await fetchImages(`https://api.pexels.com/v1/search?query=${search}`)
+
+  if (imagesResults && imagesResults.photos.length === 0) {
+    return <h2 className="m-4 text-3xl font-bold letter tracking-wider">No images found</h2>
+  }
+
+  return (
+    <>
+      {imagesResults && imagesResults.photos.length > 0 && (
+        <section className="grid grid-cols-gallery gap-4">
+          {imagesResults.photos.map((img, i) => (
+            <article
+              key={img.id}
+              className="h-64 bg-gray-200 rounded-xl overflow-hidden relative hover:scale-105 transition-all duration-300"
+            >
+              <Image
+                src={img.src.large}
+                alt={img.alt}
+                fill={true}
+                className="object-cover "
+                // sizes calculados a ojo por mi
+                // sizes="(max-width: 531px) 100vw, (max-width: 797px) 50vw, (max-width: 1063px) 33vw, 400px"
+                //sizes calculados con una web para optimizar
+                sizes="(min-width: 1420px) 272px, (min-width: 1080px) calc(5.63vw + 193px), (min-width: 800px) calc(33.08vw - 14px), (min-width: 540px) calc(50vw - 16px), calc(100vw - 16px)"
+                placeholder="blur"
+                blurDataURL={img.blurredDataUrl}
+              />
+            </article>
+          ))}
+        </section>
+      )}
+    </>
+  )
+}
